77b4f5ec489f9261bd5900c8f2092682a363dc32
Xmanvi <iredoyin15@gmail.com>
2025-07-23 23:17:00 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~hvsGzb4J#t9xwoPV-iWJC8MkA8XlD6rJXuP1z415PciFS3bXbPFU  ",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "2348129341873", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
